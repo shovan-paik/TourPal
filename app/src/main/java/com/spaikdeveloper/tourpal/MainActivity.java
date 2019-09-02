e365e6804cc2a39b3722a3b2fbf0a86a616f2783
HP <shovanpaik93@gmail.com>
2019-09-02 12:29:56 +0600

    Initial commit

---
+++ app/src/main/java/com/spaikdeveloper/tourpal/MainActivity.java
@@ -0,0 +1,64 @@
+package com.spaikdeveloper.tourpal;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
+    }
+
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.fragmentContainerFL,fragment)
+            .commit();
+            return true;
+        }
+
+
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()){
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_map:
+                fragment = new MapFragment();
+                break;
+
+            case R.id.navigation_weather:
+                fragment = new WeatherFragment();
+                break;
+
+        }
+
+        return loadFragment(fragment);
+    }
+}
---
+++ app/src/main/java/com/spaikdeveloper/tourpal/WeatherFragment.java
@@ -0,0 +1,42 @@
+package com.spaikdeveloper.tourpal;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class WeatherFragment extends Fragment {
+
+
+    public WeatherFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
